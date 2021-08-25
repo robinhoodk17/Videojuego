86af28d7143760ef9751b4631aab1ae5f2e6a013
robinhoodk17 <robinhoodk17@gmail.com>
2021-08-25 08:26:03 +0200

    the info panel for units now works as intended but looks really ugly

--- Tilemap/Assets/scripts/ScriptableObjects/Inside units/infoPanel.cs
+++ Tilemap/Assets/scripts/ScriptableObjects/Inside units/infoPanel.cs
@@ -1,15 +1,110 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class infoPanel : MonoBehaviour
 {
     public GameObject panel;
-    // Start is called before the first frame update
+    public Image sprite;
+    private unitScript unit;
+    public TextMeshProUGUI Cost;
+    public TextMeshProUGUI SUPCost;
+    public TextMeshProUGUI BarracksName;
+    public TextMeshProUGUI Level;
+    public TextMeshProUGUI XP;
+    public TextMeshProUGUI type;
+    public TextMeshProUGUI Advantages;
+    public TextMeshProUGUI Resistances;
+    public TextMeshProUGUI vulnerabilities;
+    public TextMeshProUGUI statustext; 
+    public GameObject status;
+    public GameObject attack;
+    public GameObject movement;
 
-    
+    // Start is called before the first frame update
     public void showPanel()
     {
+        unit = gameObject.GetComponent<unitScript>();
+        sprite.sprite = gameObject.transform.GetChild(3).GetComponent<SpriteRenderer>().sprite;
+        Cost.text = unit.foodCost.ToString();
+        SUPCost.text = unit.SUPCost.ToString();
+        BarracksName.text = unit.barracksname;
+        Level.text = "LV" + unit.level.ToString();
+        XP.text = "XP" + unit.levelcounter.ToString() + "/5";
+        type.text = unit.typeOfUnit;
+
+        #region advantages etc
+        string temporal = "";
+        foreach(string member in unit.advantages)
+        {
+            temporal += " " + member;
+        }
+        Advantages.text = "Adv. vs: " + temporal;
+
+        temporal = "";
+        foreach (string member in unit.resistances)
+        {
+            temporal += " " + member;
+        }
+        Resistances.text = "Res. vs: " + temporal;
+
+        temporal = "";
+        foreach (string member in unit.vulnerabilities)
+        {
+            temporal += " " + member;
+        }
+        vulnerabilities.text = "Vul. vs: " + temporal;
+        #endregion
+
+        //status
+        if (unit.status != "clear")
+        {
+            statustext.text = unit.status;
+            if (unit.status == "recovered" || unit.status == "stunned")
+            {
+                statustext.text = "stunned";
+                status.transform.GetChild(0).gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            status.transform.GetChild(0).gameObject.SetActive(false);
+            statustext.text = "clear";
+        }
+
+        //attack range and damage
+        if(unit.attackrange == 1)
+        {
+            attack.transform.GetChild(0).gameObject.SetActive(true);
+        }
+        else
+        {
+            attack.transform.GetChild(1).gameObject.SetActive(true);
+        }
+        attack.GetComponentInChildren<TextMeshProUGUI>().text = ((int)(unit.attackdamage * unit.HP / unit.maxHP)).ToString();
+
+        //movement type and distance
+        movement.GetComponentInChildren<TextMeshProUGUI>().text = unit.movement.ToString();
+        switch (unit.movementtype)
+        {
+            case ("foot"):
+                movement.transform.GetChild(0).gameObject.SetActive(true);
+                break;
+            case ("wheels"):
+                movement.transform.GetChild(1).gameObject.SetActive(true);
+                break;
+            case ("treads"):
+                movement.transform.GetChild(2).gameObject.SetActive(true);
+                break;
+            case ("flying"):
+                movement.transform.GetChild(3).gameObject.SetActive(true);
+                break;
+
+
+        }
+
         panel.SetActive(true);
     }
 
