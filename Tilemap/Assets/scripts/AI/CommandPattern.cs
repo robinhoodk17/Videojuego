358cd4ec0706359afb28262be31ff144c61b6827
robinhoodk17 <robinhoodk17@gmail.com>
2021-12-04 17:47:42 +0100

    the ai looks for the most expensive unit possbile while building

--- Tilemap/Assets/scripts/AI/CommandPattern.cs
+++ Tilemap/Assets/scripts/AI/CommandPattern.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine.Tilemaps;
 using UnityEngine;
 using TMPro;
+using System.Linq;
 
 public class CommandPattern : MonoBehaviour
 {
@@ -17,7 +18,7 @@ public class CommandPattern : MonoBehaviour
     private Camera _mainCamera;
 
     private bool firstTurn = true;
-    private Dictionary<string, GameObject> selectedbuildables;
+    private List<GameObject> selectedbuildables;
 
 
     void takeTurn()
@@ -30,10 +31,13 @@ public class CommandPattern : MonoBehaviour
 
     public void onTurnEnd()
     {
+        //we create a list of the buildable units and sort it from cheapest to most expensive
         if (firstTurn)
         {
-            selectedbuildables = mapmanager.selectedbuildables;
+            selectedbuildables = new List<GameObject>(mapmanager.selectedbuildables.Values);
+            selectedbuildables = selectedbuildables.OrderBy(go => go.GetComponent<unitScript>().foodCost).ToList<GameObject>();
         }
+
 
         if (activeplayer < maxplayers)
             activeplayer++;
@@ -51,18 +55,55 @@ public class CommandPattern : MonoBehaviour
     {
         int currentfood = mapmanager.food[1];
         int currentSUP = mapmanager.SUP[1];
+        int foodcost = 0;
+        int SUPcost = 0;
         List<GameObject> myBarracks = new List<GameObject>();
+        //we find all the barracks owned by player 2
         foreach(GameObject tile in controllables)
         {
             if (tile.GetComponent<controllable_script>().owner == 2 && map.GetTile<levelTile>(gridPosition(tile.transform.position)).type == tileType.barracks)
                 myBarracks.Add(tile);
         }
+        int numberofBarracks = myBarracks.Count;
+        unitScript[] unitstoBuild = new unitScript[numberofBarracks];
+        int j = 0;
+        //we assign the cheapest unit to build to each barrack so that all barracks build at least 1 unit.
+        for(int i = 0; i < numberofBarracks; i++)
+        {
+            if (selectedbuildables[0].GetComponent<unitScript>().foodCost + foodcost <= currentfood && selectedbuildables[0].GetComponent<unitScript>().SUPCost + SUPcost <= currentSUP)
+            {
+                unitstoBuild[i] = selectedbuildables[0].GetComponent<unitScript>();
+                foodcost += selectedbuildables[0].GetComponent<unitScript>().foodCost;
+                SUPcost += selectedbuildables[0].GetComponent<unitScript>().SUPCost;
+                j++;
+            }
+        }
+        for(int i = 0; i < j; i++)
+        {
+            foodcost -= selectedbuildables[0].GetComponent<unitScript>().foodCost;
+            SUPcost -= selectedbuildables[0].GetComponent<unitScript>().SUPCost;
+            int maxfoodcost = selectedbuildables[0].GetComponent<unitScript>().foodCost;
+            for (int k = 0; k < selectedbuildables.Count; k++)
+            {
+                if (selectedbuildables[k].GetComponent<unitScript>().foodCost + foodcost <= currentfood && selectedbuildables[k].GetComponent<unitScript>().SUPCost + SUPcost <= currentSUP)
+                {
+                    unitstoBuild[i] = selectedbuildables[k].GetComponent<unitScript>();
+                    foodcost += selectedbuildables[0].GetComponent<unitScript>().foodCost;
+                    SUPcost += selectedbuildables[0].GetComponent<unitScript>().SUPCost;
+                }
+            }
+        }
         foreach(GameObject tile in myBarracks)
         {
-            mapmanager.currentposition = gridPosition(tile.transform.position);
-            mapmanager.buildUnit(some);
-            unitname.text = "Jane";
-            accept.acceptPressed();
+            if(j>0)
+            {
+                j--;
+                mapmanager.currentposition = gridPosition(tile.transform.position);
+                mapmanager.CurrentButtonPressed = unitstoBuild[j].name;
+                mapmanager.buildUnit(unitstoBuild[j]);
+                unitname.text = unitstoBuild[j].barracksname;
+                accept.acceptPressed();
+            }
         }
 
     }
